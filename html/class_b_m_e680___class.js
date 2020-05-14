948329b64df77ea82193c4240f2f6f3edaef052c
Doxygen CI <travis@travis-ci.org>
2020-05-14 12:46:13 +0000

    Deploy code docs to GitHub Pages Travis build: 39
    Commit: b6ec02488ca5cb0758fdb7a53bc88a855fb63713

--- html/class_b_m_e680___class.js
+++ html/class_b_m_e680___class.js
@@ -5,6 +5,7 @@ var class_b_m_e680___class =
     [ "begin", "class_b_m_e680___class.html#ac0d49953ce365b412b922d396b25a83f", null ],
     [ "begin", "class_b_m_e680___class.html#ad3c587b346f158c1f65414f5b4b9de28", null ],
     [ "begin", "class_b_m_e680___class.html#ac10c361538cb76c5e643509d3740b40e", null ],
+    [ "begin", "class_b_m_e680___class.html#a9611a3516b381ee43bcfd07e282cdc89", null ],
     [ "begin", "class_b_m_e680___class.html#a7810485e014c19050198b9e7a99449b9", null ],
     [ "getSensorData", "class_b_m_e680___class.html#af0254182c826decbf2122375de32b62f", null ],
     [ "reset", "class_b_m_e680___class.html#a60539ad30b61e41dc0252b06ccc0652b", null ],
