2ef13aec3836f1571344c41f427fb8d2d7d1f13f
Doxygen CI <travis@travis-ci.org>
2020-05-25 08:54:52 +0000

    Deploy code docs to GitHub Pages Travis build: 102
    Commit: 4eaf6cfe591793cb404037f1613ed144cf2935bb

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -5,5 +5,5 @@ var searchData=
   ['avg_5fhumidity',['avg_humidity',['../_s_d_logger_s_p_i_demo_8ino.html#ae7493ad34b71d5bc3f4d6da2ce76ca9a',1,'SDLoggerSPIDemo.ino']]],
   ['avg_5fpressure',['avg_pressure',['../_s_d_logger_s_p_i_demo_8ino.html#a67acfc3f4453439da95a7bef3f19bf0e',1,'SDLoggerSPIDemo.ino']]],
   ['avg_5ftemperature',['avg_temperature',['../_s_d_logger_s_p_i_demo_8ino.html#a325c704a38de182bedbd7693692fa20e',1,'SDLoggerSPIDemo.ino']]],
-  ['arduino_20library_20to_20control_20a_20bosch_20bme680_20environmental_20sensor',['Arduino Library to control a Bosch BME680 Environmental Sensor',['../index.html',1,'']]]
+  ['arduino_20library_20to_20access_20and_20control_20a_20bosch_20bme680_20environmental_20sensor',['Arduino Library to access and control a Bosch BME680 Environmental Sensor',['../index.html',1,'']]]
 ];
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['arduino_20library_20to_20control_20a_20bosch_20bme680_20environmental_20sensor',['Arduino Library to control a Bosch BME680 Environmental Sensor',['../index.html',1,'']]]
+  ['arduino_20library_20to_20access_20and_20control_20a_20bosch_20bme680_20environmental_20sensor',['Arduino Library to access and control a Bosch BME680 Environmental Sensor',['../index.html',1,'']]]
 ];
