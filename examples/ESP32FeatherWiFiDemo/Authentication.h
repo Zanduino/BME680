f1b5e19789992ff35062ed7d1e1047298789e794
Zanshin <Zanshin_Github@SV-Zanshin.com>
2020-05-31 13:53:18 +0200

    Corrected Data

--- examples/ESP32FeatherWiFiDemo/Authentication.h
+++ examples/ESP32FeatherWiFiDemo/Authentication.h
@@ -1,3 +1,4 @@
 #pragma once
-const char* WIFI_SSID     = "YourNetworkNameGoesHere"; ///< Network to connect to
-const char* WIFI_PASSWORD = "EnterPasswordHere";       ///< Network authentication code
+const char* WIFI_SSID = "Zanshin";     ///< Network to connect to
+const char* WIFI_PASSWORD = "TANSTAAFl1!"; ///< Network authentication code
+
