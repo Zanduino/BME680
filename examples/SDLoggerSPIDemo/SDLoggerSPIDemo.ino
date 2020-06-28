12db936ec1aae1079cf66b62852ca9002334837d
Zanshin <Zanshin_Github@SV-Zanshin.com>
2020-06-28 19:17:27 +0200

    Correct documentation

--- examples/SDLoggerSPIDemo/SDLoggerSPIDemo.ino
+++ examples/SDLoggerSPIDemo/SDLoggerSPIDemo.ino
@@ -130,9 +130,7 @@ const uint16_t HUMIDITY_TRIP      = 1000;           ///< Trigger if delta is mor
 **************************************************************************************************/
 BME680_Class BME680;    ///< Create an instance of the BME680 class
 File         dataFile;  ///< Class for a SD-Card file
-/*************************************************************************
-** Structure type definition contains a set of readings from the BME680 **
-*************************************************************************/
+/*! Structure "reading" contains a set of readings from the BME680 */
 struct reading {
   int32_t temperature;            ///< temperature in deci-degrees
   int32_t humidity;               ///< humidity in milli-percent
